b6673600caf6a9a530a17a2fb4efa7d8647f970c
petziferum <daboarderpjb@gmail.com>
2023-10-07 19:18:29 +0200

    petziferums helloworld

---
+++ Scripts/petziferum.js
@@ -0,0 +1,10 @@
+// My Contribution to Hacktoberfest 2023
+    console.log("Hello World!");
+
+export function helloWorldHtml() {
+      return (
+          <div>
+              <h1>Hello World!</h1>
+          </div>
+      );
+  }
