0404f13581745b7c7c65f71f208a6cb45be35e54
Srepadmashiny Karthikeyasankar <133505398+popshiny-0906@users.noreply.github.com>
2023-10-12 00:52:47 +0530

    I have updated the scripts part
    popshiny0906

---
+++ Scripts/SrePadmashiny.py
@@ -0,0 +1,7 @@
+// LANGUAGE: Python
+// AUTHOR: Sre Padmashiny K
+// GITHUB: https://github.com/popshiny-0906
+
+# This program is in python programming language!
+
+print('Hello this is Sre Padmashiny, I am very excited to contribute to Hacktoberfest 2023!')
