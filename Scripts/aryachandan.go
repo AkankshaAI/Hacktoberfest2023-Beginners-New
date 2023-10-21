fc290c5d3e716bf5a5a0ff0b89fa15e5fb4db08d
Chandan <97820157+alpha2lucifer@users.noreply.github.com>
2023-10-21 10:25:02 +0530

    Create aryachandan.go
    Added a simple "Hello, World!" script in Go
    
    The script file 'hello_world.go' contains a basic Go program that utilizes the 'fmt' package to print the string "Hello, World!" to the console. Explanatory comments have been included to enhance code readability and understanding.
    
    This commit introduces a foundational example of a Go script, providing a clear demonstration of the basic syntax and structure of a Go program. The explanatory comments aim to assist beginners in understanding the functionality of the script and the role of each component.
    
    The script has been thoroughly tested using the 'go run' command to ensure the successful execution of the "Hello, World!" program. The output has been verified to confirm that the script prints the expected message to the console.
    
    This commit adheres to the standard coding conventions and best practices for Go programming. It is ready for review and integration into the main codebase.

---
+++ Scripts/aryachandan.go
@@ -0,0 +1,12 @@
+// This is a simple "Hello, World!" program in Go.
+// The main package is required for Go executable programs.
+package main
+
+// Importing the 'fmt' package to use the Println function for printing output.
+import "fmt"
+
+// The main function is the entry point of the executable program.
+func main() {
+	// The Println function from the 'fmt' package prints the string "Hello, World!" to the console.
+	fmt.Println("Hello, World!")
+}
