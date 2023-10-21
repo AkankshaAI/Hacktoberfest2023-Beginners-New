cda681e151217df24b3ee06364a694ace0c81685
Chetna Sharma <146471211+chetnadev@users.noreply.github.com>
2023-10-21 22:46:24 +0530

    Create chetna.js
    Variables and Data Types example

---
+++ chetna.js
@@ -0,0 +1,17 @@
+// LANGUAGE: javascript
+// AUTHOR: Chetna Sharma
+// GITHUB: https://github.com/chetnadev
+
+# This program is in javascript!
+//Declaring variables and using different data types
+// Variables
+let name = "chetna";
+const age = 26;
+var isStudent = true;
+
+// Data Types
+let fruit = "Apple"; // String
+let count = 10;      // Number
+let isRipe = true;   // Boolean
+let person = { name: "keshav", age: 28 }; // Object
+
