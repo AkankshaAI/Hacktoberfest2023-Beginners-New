2598eba7f333ff68c867ddbae59628b6ab8e5b57
DEEPA RAWAT <39901983+Deeparawat@users.noreply.github.com>
2023-10-24 00:28:49 +0530

    deepa.cpp
    intro print message added

---
+++ Scripts/deepa.cpp
@@ -0,0 +1,9 @@
+#code to print your message in c++
+
+#include<iostream>
+using namespace std;
+
+int main(){
+cout<<"Hello participants welcome to hacktoberfest-2023 , I am Deepa this side !.";
+return 0;
+}
