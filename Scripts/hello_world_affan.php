8f2d400f4865da695f1d7e6715324d6186289505
Affan Ali <affanali577@gmail.com>
2023-10-24 13:32:09 +0500

    adding hello world message in PHP by using custom function

---
+++ Scripts/hello_world_affan.php
@@ -0,0 +1,12 @@
+<?php 
+// printing hello world message by using custom function
+
+
+function print_message($var) {
+    echo"". $var ."";
+}
+
+print_message("Hello World - By Affan Ali");
+
+
+?>
