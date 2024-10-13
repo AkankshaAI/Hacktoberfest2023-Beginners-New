6e1da47d74fa6d3a36fb517455dca90603c20255
AkShAy21211 <akshay68580@gmail.com>
2024-10-13 11:34:54 +0530

    Improved clock function formatting and structure

--- JS projects/Countdown Timer/index.js
+++ JS projects/Countdown Timer/index.js
@@ -1,24 +1,26 @@
-const endDate="31 October 2023 11:59 PM"
+const endDate = "31 October 2023 11:59 PM";
 document.getElementById("end-date").innerText = endDate;
 
-const inputs=document.querySelectorAll("input")
-function clock(){
-    const end=new Date(endDate);
-    const now=new Date()
-    
-    const diff=(end-now)/1000;
-    if(diff<0) return;
-    //convert into days
-    inputs[0].value=Math.floor(diff/3600/24);
-    inputs[1].value=Math.floor((diff/3600)%24);
-    inputs[2].value=Math.floor((diff/60)%60);
-    inputs[3].value=Math.floor(diff%60);
+const inputs = document.querySelectorAll("input");
+
+function formatTime(timeValue) {
+  return Math.floor(timeValue);
 }
-//initial call
-clock()
-setInterval(
-    ()=>{
-        clock();
-    },
-    1000
-)
+
+function clock() {
+  const end = new Date(endDate);
+  const now = new Date();
+
+  const diff = (end - now) / 1000;
+  if (diff < 0) return;
+
+  inputs[0].value = formatTime(diff / 3600 / 24);  // Days
+  inputs[1].value = formatTime((diff / 3600) % 24);  // Hours
+  inputs[2].value = formatTime((diff / 60) % 60);  // Minutes
+  inputs[3].value = formatTime(diff % 60);  // Seconds
+}
+
+// Initial call
+clock();
+
+setInterval(clock, 1000);
